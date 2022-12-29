3db81a57f797b432423eec0769e70d53217754fe
Antony Alkmim <antony.alkmim@stone.com.br>
2022-12-29 11:40:04 -0300

    chore: initial structure

---
+++ Core/Core/MyView.swift
@@ -0,0 +1,12 @@
+//
+//  MyView.swift
+//  Core
+//
+//  Created by antony.alkmim on 29/12/22.
+//
+
+import Foundation
+
+public enum MyView {
+    case start, list
+}
---
+++ FeatureGames/FeatureGames/FeatureGames.h
@@ -0,0 +1,18 @@
+//
+//  FeatureGames.h
+//  FeatureGames
+//
+//  Created by antony.alkmim on 29/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeatureGames.
+FOUNDATION_EXPORT double FeatureGamesVersionNumber;
+
+//! Project version string for FeatureGames.
+FOUNDATION_EXPORT const unsigned char FeatureGamesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureGames/PublicHeader.h>
+
+
