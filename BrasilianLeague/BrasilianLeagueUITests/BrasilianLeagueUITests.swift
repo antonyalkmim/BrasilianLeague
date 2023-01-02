5917ea903145acb3f888ab93e38903d197c864d8
Antony Alkmim <antony.alkmim@stone.com.br>
2023-01-02 16:28:56 -0300

    tests: force AllTests run UItests

--- BrasilianLeague/BrasilianLeagueUITests/BrasilianLeagueUITests.swift
+++ BrasilianLeague/BrasilianLeagueUITests/BrasilianLeagueUITests.swift
@@ -19,15 +19,5 @@ final class BrasilianLeagueUITests: XCTestCase {
     func testExample() throws {
         let app = XCUIApplication()
         app.launch()
-
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
